854fb2a812926b29fa150950674438d9084cd833
Alan Portilla <portillacastillo56@gmail.com>
2024-03-15 17:10:14 -0500

    Creacion del diagrama completo

--- academics/models.py
+++ academics/models.py
@@ -12,29 +12,16 @@ class User(models.Model):
 
 class Students(models.Model):
     code = models.CharField(max_length = 50)
-    id_person = models.IntegerField()
+    id_person = models.ForeignKey('Persons', on_delete=models.CASCADE, blank=False)
     status = models.BooleanField(default = True)
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null =True, blank = True)
 
-class Identifications_type(models.Model):
+class Identification_type(models.Model):
     name = models.CharField(max_length = 50)
     abrev = models.CharField(max_length = 10)
     descrip = models.CharField(max_length = 100)
-    created_at = models.DateTimeField(default = datetime.datetime.now())
-    updated_at = models.DateTimeField(default = datetime.datetime.now())
-    deleted_at = models.DateTimeField(null =True, blank = True)
-
-class Persons(models.Model):
-    first_name = models.CharField(max_length = 50)
-    last_name = models.CharField(max_length = 50)
-    id_ident_type = models.IntegerField()
-    ident_number = models.CharField(max_length =15)
-    id_exp_city = models.IntegerField()
-    address = models.CharField(max_length =150)
-    mobile = models.CharField(max_length =50)
-    id_user = models.IntegerField()
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null =True, blank = True)
@@ -43,7 +30,20 @@ class Cities(models.Model):
     name = models.CharField(max_length = 100)
     abrev = models.CharField(max_length =10)
     descrip = models.CharField(max_length =10)
-    id_dept = models.IntegerField()
+    id_dept = models.ForeignKey('Deparments', on_delete=models.CASCADE, blank=False)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null =True, blank = True)
+
+class Persons(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    id_ident_type = models.ForeignKey('Identification_type', on_delete=models.CASCADE, blank=False)
+    ident_number = models.CharField(max_length =15)
+    id_exp_city = models.ForeignKey('Cities', on_delete=models.CASCADE, blank=False)
+    address = models.CharField(max_length =150)
+    mobile = models.CharField(max_length =50)
+    id_user = models.ForeignKey('User', on_delete=models.CASCADE, blank=False)
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null =True, blank = True)
@@ -52,7 +52,7 @@ class Deparments(models.Model):
     name = models.CharField(max_length = 100)
     abrev = models.CharField(max_length =10)
     descrip = models.CharField(max_length =10)
-    id_country = models.IntegerField()
+    id_country = models.ForeignKey('Countries', on_delete=models.CASCADE, blank=False)
     created_at = models.DateTimeField(default = datetime.datetime.now())
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null =True, blank = True)
