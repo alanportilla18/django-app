42f4ff2873e4615004dc0a6762f6b232f6c904b8
Alan Portilla <portillacastillo56@gmail.com>
2024-03-17 21:04:16 -0500

    generar interfaces graficas automaticamente en el CRUD para cada modelo

--- academics/admin.py
+++ academics/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Identification_type, Countries, Deparments, Cities, Persons, Students
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Identification_type)
+admin.site.register(Countries)
+admin.site.register(Deparments)
+admin.site.register(Cities)
+admin.site.register(Persons)
+admin.site.register(Students)
